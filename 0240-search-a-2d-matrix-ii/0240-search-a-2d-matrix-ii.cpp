135f1916c90f72850189e85edbead33242a135e3
Prakhar Saxena <lkbprakhar27@gmail.com>
2026-08-26 18:52:30 +0530

    Time: 55 ms (50.42%), Space: 18.6 MB (69.12%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -6,13 +6,13 @@ public:
         int i = 0;        
         int j = m - 1;    
         while (i < n && j >= 0) {
-            if (matrix[i][j] == target) {
+            if (matrix[i][j] == target) 
                 return true;
-            } else if (matrix[i][j] < target) {
+            else if (matrix[i][j] < target) 
                 i++;  
-            } else {
+             else 
                 j--; 
-            }
+            
         }
         return false;
     }
