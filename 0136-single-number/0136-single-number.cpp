62773da645268970f0637f9a1d8639df55b6bc9d
Prakhar Saxena <lkbprakhar27@gmail.com>
2026-08-26 13:01:50 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.13%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        int max_val = *max_element(nums.begin(), nums.end());
-        int min_val = *min_element(nums.begin(), nums.end());
-
-        vector<int> count(max_val - min_val + 1, 0);
-        for (int i = 0; i < n; i++) {
-            count[nums[i] - min_val]++;
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
         }
-        for (int i = 0; i < n; i++) {
-            if (count[nums[i] - min_val] == 1) {
-                return nums[i];
-            }
-        }
-        return -1; 
+        return result;
     }
 };
