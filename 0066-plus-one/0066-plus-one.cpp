cae0ee491eada1ea51103c4309f576588a144c5d
Prakhar Saxena <lkbprakhar27@gmail.com>
2026-08-28 21:36:44 +0530

    Time: 0 ms (100%), Space: 11.6 MB (61.04%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        // Traverse from the last digit
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;  
+                return digits;   
+            }
+            digits[i] = 0;        
+        }      
+       digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
