a00fdc1781479908c7d13854df6e8163eee3c9d5
Prakhar Saxena <lkbprakhar27@gmail.com>
2026-09-17 20:02:01 +0530

    Time: 0 ms (100%), Space: 76.9 MB (68.63%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        while (head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+
+        ListNode* result = nullptr;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) { sum += l1->val; l1 = l1->next; }
+            if (l2) { sum += l2->val; l2 = l2->next; }
+            carry = sum / 10;
+            ListNode* node = new ListNode(sum % 10);
+            node->next = result;   
+            result = node;
+        }
+        return result;
+    }
+};
